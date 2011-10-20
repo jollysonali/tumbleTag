128585ef6af6a6e8117e5e761a92632c44757216
Sonali Sridhar <sonali.sridhar@gmail.com>
2011-10-20 17:27:58 -0400

    Created a custom selector for a case insensitive href match.
    Added a function to the plugin to deal with the fact that .find() is
    case sensitive and tags shouldn't be.

--- tumbleTag.js
+++ tumbleTag.js
@@ -1,12 +1,22 @@
 (function($) {
-	$.fn.tumbleTag = function(subdomain, api_key) {
+	//This custom selector for a case insensitive href match.
+	$.extend($.expr[':'], {
+	      'containshref': function(elem, i, match, array)
+	      {
+	          var $e=$(elem);
+	        return ($e.attr('href') || '').toLowerCase()
+	        .indexOf((match[3] || "").toLowerCase()) >= 0;
+	      }
+	    });
+	
+	$.fn.tumbleTag = function(subdomain, api_key) {	
 		var $ul = this.first();
 		var url = 'http://api.tumblr.com/v2/blog/' + subdomain + '/posts?api_key=' + api_key + '&jsonp=?';
 	
 		$.getJSON(url, function(data) {
 			$.each(data.response.posts, function(idx, post) {
 				$.each(post.tags, function(idx, tag) {
-					if ($ul.find('a[href="/tagged/' + tag + '"]').length == 0) {
+					if ($ul.find('a:containshref(' + tag + ')').length == 0) {
 		                $ul.append($('<li />').append($('<a />').attr('href', '/tagged/' + tag).text(tag)));
 		            }		
 				});
@@ -19,3 +29,5 @@
 
 // how you use it
 //$(".tag_list").tumbleTag('savesafe.tumblr.com', 'API KEY');
+
+
